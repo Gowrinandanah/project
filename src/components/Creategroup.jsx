47b169538166bed5157560143895b5bd44c2e7d8
Gowrinandanah <nandanag625@gmail.com>
2025-06-24 13:06:00 +0530

    Initial commit

---
+++ src/components/Creategroup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Creategroup = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Creategroup
---
+++ src/components/Groupdetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Groupdetails = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Groupdetails
